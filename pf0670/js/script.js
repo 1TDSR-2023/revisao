5850048d38ee746d16336825c3b876b8fe5548b4
Marlyemarcio1 <luan.rdias@gmail.com>
2023-08-02 22:06:11 -0300

    let e for

---
+++ pf0670/js/script.js
@@ -0,0 +1,10 @@
+// console.log("hello world")
+
+// Declara um array de Sting de alunos
+let alunos = ["João", "Maria", "José", "Pedro", "Ana"]
+// Imprimir o Array de alunos
+console.log(alunos[0]);
+// Imprimir todos os elementos do array de alunos
+for(let i = 0; i <alunos.length;i++){
+    console.log(alunos,{i});
+}
