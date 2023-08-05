7a5e4b220c4c681080a2e45a349468707b805a18
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-08-04 21:59:58 -0300

    Lista criada, Listener do btnAddTarefa criado-ok.

---
+++ pf0670/js/tarefas.js
@@ -0,0 +1,19 @@
+
+//Criando a lista de tarefas array vazia...
+let listaTarefasArray = [];
+
+//Recuperando o botão do formulário que vai iniciar o processo de inserção da tarefa.
+const botaoAddTarefa = document.querySelector("#btnAddTarefa");
+
+//Adicionando um listener ao botão para esperar um clique que vai disparar a ação.
+botaoAddTarefa.addEventListener("click", (evento)=>{
+    evento.preventDefault();
+    //Pegando os valores dos campos
+    const inputTarefa = document.querySelector("#idTarefa");
+    
+    //Inserindo a nova tarefa no array com o método push();
+    listaTarefasArray.push(inputTarefa.value);
+        
+    console.log(listaTarefasArray);
+    inputTarefa.value = "";
+});
