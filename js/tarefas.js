43801573d93659e7391de893c9ffc19956b99687
K413l <kalel.schlichting@gmail.com>
2023-08-04 21:59:33 -0300

    criando botao

--- js/tarefas.js
+++ js/tarefas.js
@@ -4,6 +4,9 @@ const botaoAddTarefa = document.querySelector("#btnAddTarefa");
 
 botaoAddTarefa.addEventListener("click",(evento)=>{
     const inputTarefa = document.querySelector("#idTarefa");
+    
+    listaTarefasArray.push(inputTarefa.value)
+    
     console.log(inputTarefa.value);
     inputTarefa.value = "";
 });
