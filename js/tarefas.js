f02e05475e65cc980c151647290080715d38fb3a
PhabloFiap <rm550687@fiap.com.br>
2023-08-04 23:02:40 -0300

    conseguindo remover

--- js/tarefas.js
+++ js/tarefas.js
@@ -13,7 +13,52 @@ botaoAddTarefa.addEventListener("click", (evento)=>{
     
     //Inserindo a nova tarefa no array com o método push();
     listaTarefasArray.push(inputTarefa.value);
+    
+    //Recuperando a lista UL através do id com querySelector();
+    const listaTarefasUL = document.querySelector("#lista-tarefas");
+
+    //Elemento li criado.
+    let li = document.createElement("li");
+
+    //Adiocionando texto dentro do elemento LI;
+    li.textContent = inputTarefa.value;
+
+    //Adicionando o elemento li a UL.
+    listaTarefasUL.appendChild(li);
+
+    //Criando o bot"ao de excluir tarefas.
+    let botaoApagaTarefa = document.createElement("button");
+
+    //Inserindo o nó de texto do botão
+    botaoApagaTarefa.textContent = " x ";
+
+    //Adicionando um botão para apagar a tarefa!
+    li.appendChild(botaoApagaTarefa);
+
+    botaoApagaTarefa.addEventListener("click",(e)=>{
+
         
+
+        listaTarefasArray.forEach( tarefa =>{
+            if(tarefa == e.target.parentNode.textContent.split(" ")[0]){
+
+                listaTarefasArray.splice(listaTarefasArray.indexOf(tarefa),1);
+            }
+
+        });
+
+        //Removendo do HTML
+        e.target.parentNode.remove();
+
+        console.log(e.target.parentNode.textContent.split(" ")[0]);
+
+
+    });
+    
+
+
     console.log(listaTarefasArray);
     inputTarefa.value = "";
 });
+
+
