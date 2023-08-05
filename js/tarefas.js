cdc96161685edf5e0c565c789ee129bd631014e2
K413l <kalel.schlichting@gmail.com>
2023-08-04 22:05:31 -0300

    update js

--- js/tarefas.js
+++ js/tarefas.js
@@ -1,12 +1,23 @@
-let listaTarefasArray =[];
 
+//Criando a lista de tarefas array vazia...
+let listaTarefasArray = [];
+
+//Recuperando o botão do formulário que vai iniciar o processo de inserção da tarefa.
 const botaoAddTarefa = document.querySelector("#btnAddTarefa");
 
-botaoAddTarefa.addEventListener("click",(evento)=>{
+//Adicionando um listener ao botão para esperar um clique que vai disparar a ação.
+botaoAddTarefa.addEventListener("click", (evento)=>{
+    evento.preventDefault();
+    //Pegando os valores dos campos
     const inputTarefa = document.querySelector("#idTarefa");
     
-    listaTarefasArray.push(inputTarefa.value)
-    
-    console.log(inputTarefa.value);
+    //Inserindo a nova tarefa no array com o método push();
+    listaTarefasArray.push(inputTarefa.value);
+
+    const listaTarefasUL = document.querySelector("#lista-tarefas");
+
+    let li = document.createElement("li");
+        
+    console.log(listaTarefasArray);
     inputTarefa.value = "";
 });
