b4a1c1d575bec67713fd753be8062bae0b4e2eb5
Pedro-Ferrari <pedrofcmp@gmail.com>
2023-08-10 13:59:19 -0300

    Arrumando o código. js

--- js/arrays.js
+++ js/arrays.js
@@ -1,29 +1,38 @@
-const formTarefas = document.getElementById('form-tarefas');
-const formLista = document.getElementById('form-lista');
-const input = document.getElementById('caixa');
+const taskForm = document.getElementById('taskForm');
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+const tasks = [];
 
-const listaTarefas = [];
-
-function adicionarTarefa() {
-    const formTarefas = input.value;
-        if (formTarefas){
-            form.push(formTarefas);
-            renderform();
-            formInput.value = '';
-        }
-}
-
-function removeForm(index){
-    forms.splice (index, 1);
-    renderform();
-}
-
-function renderform(){
-    formlist.innerHTML = '';
-    for(let i = 0; i < form.length; i++ ){
-        const li = document.createElement('li');
-        li.textContent = form[i];
-        const removeButton = document.createElement('button');
-        removeButton.textContent = '*'
+function addTask() {
+    const taskText = taskInput.value;
+    if (taskText) {
+        tasks.push(taskText);
+        renderTasks();
+        taskInput.value = '';
     }
 }
+
+function removeTask(index) {
+    tasks.splice(index, 1);
+    renderTasks();
+}
+
+function renderTasks() {
+    taskList.innerHTML = '';
+    for (let i = 0; i < tasks.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = tasks[i];
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'X';
+        removeButton.addEventListener('click', () => removeTask(i));
+        li.appendChild(removeButton);
+        taskList.appendChild(li);
+    }
+}
+
+taskForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    addTask();
+});
+
+renderTasks();
